2351697da61f154162610ccd5f3293ee8fe3e88f
William Park <89389554+willhwpark@users.noreply.github.com>
2023-12-13 03:11:42 -0800

    first commit

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,16 @@
+import CategoryItem from "../category-item/category-item.component";
+import "./directory.styles.scss";
+
+const Directory = ({categories}) => {
+  return (
+    <div>
+    {categories.map((category) => {
+    return (
+      <CategoryItem key={category.id} category={category} />
+    )
+  })}
+</div>
+  )
+}
+
+export default Directory;
